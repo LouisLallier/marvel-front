18ca878d83fa255dfaa7a36775462249fad64257
louislallier <louis.lallier@gmail.com>
2023-02-20 19:31:27 +0100

    final details

--- src/pages/OneComic.js
+++ src/pages/OneComic.js
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { useMemo } from "react";
